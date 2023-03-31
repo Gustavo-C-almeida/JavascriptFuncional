ea2646b384efabdab5ba0142deb045be11c29373
Gustavo-C-almeida <105670387+Gustavo-C-almeida@users.noreply.github.com>
2023-03-30 22:21:00 -0300

    Commit
    Proximo: função filter

--- Funções/Map/map.js
+++ Funções/Map/map.js
@@ -1,3 +1,25 @@
-const nums = [1,2,3]
-const res = n => n * 2
-console.log(nums.map(res))
+const carrinho = [
+    {nome: "caneta", quant: 12, preco: 8.99},
+    {nome: "impressora", quant: 2, preco: 380},
+    {nome: "Estojo", quant: 1, preco: 12}
+]
+
+
+nome = produto => produto.nome
+total = produto => produto.preco*produto.quant
+console.log(carrinho.map(nome))
+console.log(carrinho.map(total))
+
+
+Array.prototype.meuMap = function(fn){
+    const novoArray = []
+    for(let i = 0; i<this.length;i++){
+        const res = fn(this[i], i, this)
+        novoArray.push(res)
+    }
+    return novoArray
+}
+nome = produto => produto.nome
+total = produto => produto.preco*produto.quant
+console.log(carrinho.meuMap(nome))
+console.log(carrinho.meuMap(total))
