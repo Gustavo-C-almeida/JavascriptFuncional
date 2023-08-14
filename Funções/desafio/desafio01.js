44c1891a0d2684e097d4c1be8bbcc98bb95ef557
Gustavo-C-almeida <105670387+Gustavo-C-almeida@users.noreply.github.com>
2023-08-14 18:45:10 -0300

    Minha parte do desafio
    Minha parte do desafio, continuar fazendo desafio porém vendo o video de correção

--- Funções/desafio/desafio01.js
+++ Funções/desafio/desafio01.js
@@ -1,18 +1,31 @@
+
 const carrinhoDeCompras = [
     { nome: "Arroz", preco: 5.99, quantidade: 2, fragil: false },
     { nome: "Leite", preco: 3.50, quantidade: 3, fragil: false },
     { nome: "Maçã", preco: 2.50, quantidade: 30, fragil: true },
     { nome: "Bolo", preco: 12.50, quantidade: 5, fragil: true },
     { nome: "Brigadeiro", preco: 0.50, quantidade: 120, fragil: true }
-    // Adicione mais produtos conforme necessário
-];
-const media = carrinhoDeCompras
-.filter(item => item.fragil)
-.map(item => item.preco * item.quantidade)
+]
 
-console.log(media)
+//minha versão sem ver o video
+const soma = (acc, el) => acc+el
+
+const frageis = carrinhoDeCompras
+.filter(item => item.fragil);
+
+for(i = 0; i < frageis.length; i++){
+    quant = frageis[i].quantidade
+    preco = frageis[i].preco
+    tot = quant*preco
+    console.log(quant, preco, tot)
+}
+
+ const media = (frageis.map(item => item.preco*item.quantidade)
+ .reduce(soma))/frageis.length
+
+ console.log(media)
+ //versão corrigida com o video:
  
-// Falta o reduce que deve mostrar a quantidade de itens, valor final Total e media dos preços
 
 
 
